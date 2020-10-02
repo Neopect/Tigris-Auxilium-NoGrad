27181e567541e68c8d416c391f184d626dabe3dd
Neopect <tyler.moen@earthlink.net>
2020-10-01 20:30:10 -0500

    working on tables

--- src/Controllers/HomeworkCon.java
+++ src/Controllers/HomeworkCon.java
@@ -1,4 +1,55 @@
 package Controllers;
 
+import features.SqliteConnection;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class HomeworkCon {
+    public Button btn1;
+    public TableView ppTable;
+    public TextField tfID;
+    public TextField tdTitle;
+    public TextField tfAuthor;
+    public TextField tfYear;
+    public TextField tfPages;
+    public TableView tbBooks;
+    public TableColumn colID;
+    public TableColumn colTitle;
+    public TableColumn colAuthor;
+    public TableColumn colYear;
+    public TableColumn colPages;
+
+    Connection connection = null;
+
+    public void initialize() {
+        SqliteConnection sqlConn = new SqliteConnection();
+        connection = sqlConn.dbConnector("ProjectPlanner");
+    }
+
+    public void btn1Action(ActionEvent actionEvent) {
+        try {
+            String query = "select * from ProjectInfo"; //If you want all use * "select * from "
+            PreparedStatement pst = connection.prepareStatement(query);
+            ResultSet rs = pst.executeQuery();
+            System.out.println(rs);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertAction(ActionEvent actionEvent) {
+    }
+
+    public void updateAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteAction(ActionEvent actionEvent) {
+    }
 }
--- src/Controllers/SetupCon.java
+++ src/Controllers/SetupCon.java
@@ -1,10 +1,17 @@
 package Controllers;
 
 import features.RWJsonUser;
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,14 +23,24 @@ public class SetupCon {
 
     public TextField txtBoxFN;
     public TextField txtBoxLN;
-    public ChoiceBox comboGrade;
+    public ChoiceBox<String> comboGrade;
     public TextField txtBoxISD;
+
+    public void initialize() {
+        ObservableList<String> list = comboGrade.getItems();
+        list.add("9");
+        list.add("10");
+        list.add("11");
+        list.add("12");
+
+    }
+
 
     public void FinishedClicked(ActionEvent actionEvent) {
 
         RWJsonUser.firstName = txtBoxFN.getText();
         RWJsonUser.lastName = txtBoxLN.getText();
-        //RWJsonUser.sGrade = comboGrade.getSelectionModel().toString();
+        RWJsonUser.sGrade = comboGrade.getSelectionModel().toString();
         RWJsonUser.sIsd = txtBoxISD.getText();
         RWJsonUser.getComputerName(false);
         RWJsonUser.getOSVersion();
@@ -33,6 +50,17 @@ public class SetupCon {
 
         RWJsonUser.WriteToJson();
         RWJsonUser.ReadToJson();
+
+        try {
+            Parent home_page_parent = FXMLLoader.load(getClass().getResource("/fxml/Home.fxml"));
+            Scene home_page_scene = new Scene(home_page_parent);
+            Stage app_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            app_stage.setScene(home_page_scene);
+            app_stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
